98c403f7ac79e3e2b8da3603f26a67d7dc1109d5
Hosaina Y <yhosaina@outlook.com>
2026-01-08 12:24:28 +0300

    Resolve merge conflicts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bolemedhanialem.mezmurtinat',
+  appName: 'yetimket mezmurat',
+  webDir: 'dist'
+};
+
+export default config;
